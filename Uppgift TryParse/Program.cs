fc029b16cbdf051fe703e043dec225fd754fd8e0
ErnstOsbeck <143172129+ErnstOsbeck@users.noreply.github.com>
2024-10-24 12:14:24 +0200

    Add project files.

---
+++ Uppgift TryParse/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace uppgift
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int resultat;
+            if (!TryParse(Console.ReadLine(), out resultat))
+            {
+                Console.WriteLine("Skriv in en siffra");
+            }
+        }
+        public static bool TryParse(string s, out int result)
+        {
+            
+            bool resultat;
+            try
+            {
+                result = Convert.ToInt32(s);
+                resultat = true;
+                return resultat;
+            }
+            catch
+            {
+                result = 0;
+                resultat = false;
+                return resultat;
+            }
+        }
+    }
+}
